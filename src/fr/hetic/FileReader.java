419d4b6c451aa8f96689f87b336bd8c4975be907
Zoltrak <a_lanyan@hetic.eu>
2024-04-10 16:49:08 +0200

    amelioration du reader de l'exo 2

--- src/fr/hetic/FileReader.java
+++ src/fr/hetic/FileReader.java
@@ -1,24 +1,60 @@
+import java.io.*;
 package fr.hetic;
 
-class FileReader{
+public class Main {
     public static void main(String[] args) {
 
-        // Creation d'un filereader
-        try {
-            FileReader fileReader = new FileReader(/learning-java/src/fr/hetic/textfile/results.txt);
-            BufferedReader reader = new BufferedReader(fileReader);
+        if (args.length ==0) {
+            System.out.println("Préciser le chemin du dossier");
+            return;
 
-            //affichage de la ligne
-            String line = reader.readline();
-            while (line =! null) {
-                System.out.println(line);
-                //lecture de la prochaine ligne
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        string dossier = args[0];
+        File dossierFichiers = new File(textfile);
+        File[] fichiers = dossierFichiers.listFiles((dir, name) -> name.endsWith(".op"));
 
+        for (File fichier : fichiers) {
+            String fichierResultat = fichier.getAbsolutePath().replace(".op,", ".res");
+       try (BufferedReader reader = new BufferedReader(new FileReader(fichier));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fichierResultat))) {
+
+                String ligne;
+                while ((ligne = reader.readLine()) != null) {
+                    String[] elements = ligne.split(";");
+                    if (elements.length != 3) {
+                        writer.write("ERROR");
+                        writer.newLine();
+                        continue;
+                    }
+
+                    try {
+                        nombre1 = Double.parseDouble(args[0]);
+                        nombre2 = Double.parseDouble(args[1]);
+                        operateur = args[2];
+                    } catch (NumberFormatException e) {
+                        System.out.println ("Les deux premiers arguments doivent êtres des nombres");
+                        return;
+                    }
+
+                    double resultat = 0;
+                    switch (operateur) {
+                        case "+":
+                            resultat = nombre1 + nombre2;
+                            break;
+                        case "*":
+                            resultat = nombre1 * nombre2;
+                            break;
+                        case "-":
+                            resultat = nombre1 - nombre2;
+                            if (nombre1 > nombre2) { resultat = nombre1 - nombre2; }
+                            else { resultat = nombre2 - nombre1; }
+                            break;
+                    }
+
+                    System.out.println ("Résultat de l'opération :" + resultat);
+
+                }
+
+           }
         }
     }
