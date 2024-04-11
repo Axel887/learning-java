5454b064df125cf01dd82844b80e2be983e013bf
Zoltrak <a_lanyan@hetic.eu>
2024-04-11 16:49:49 +0200

    programmation fonctionnelle with Hashmap

--- src/fr/hetic/Calculateur.java
+++ src/fr/hetic/Calculateur.java
@@ -1,47 +1,36 @@
-package fr.hetic;
+import java.util.HashMap;
 
-/*Écrire un programme de type console Calculateur.java prenant 3 paramètres
-(numérique, numérique, chaîne de caractères) qui, après avoir validé les entrées affichera
-le résultat de l’opération mathématique décrite.
-Liste des opérateurs à prendre en compte : + - * */
-
-class Calculateur {
+public class Calculateur {
     public static void main(String[] args) {
         if (args.length != 3) {
-            System.out.println (" Le calculateur a besoin de 3 paramètres pour fonctionner <nombre> <nombre> <opérateur> ");
+            System.out.println("Le calculateur a besoin de 3 paramètres pour fonctionner <nombre> <nombre> <opérateur>");
             return;
         }
-       double nombre1, nombre2;
-       String  operateur;
+
+        double nombre1, nombre2;
+        String operateur;
 
         try {
             nombre1 = Double.parseDouble(args[0]);
             nombre2 = Double.parseDouble(args[1]);
             operateur = args[2];
         } catch (NumberFormatException e) {
-            System.out.println ("Les deux premiers arguments doivent êtres des nombres");
+            System.out.println("Les deux premiers arguments doivent être des nombres");
             return;
         }
 
-        double resultat = 0;
-        switch (operateur) {
-            case "+":
-             resultat = nombre1 + nombre2;
-                    break;
-            case "*":
-             resultat = nombre1 * nombre2;
-                    break;
-            case "-":
-             resultat = nombre1 - nombre2;
-             if (nombre1 > nombre2) { resultat = nombre1 - nombre2; }
-                    else { resultat = nombre2 - nombre1; }
-                    break;
+        HashMap<String, Operation> operations = new HashMap<>();
+        operations.put("+", new Addition());
+        operations.put("-", new Soustraction());
+        operations.put("*", new Multiplication());
+
+        Operation operation = operations.get(operateur);
+        if (operation == null) {
+            System.out.println("Opérateur non supporté");
+            return;
         }
 
-        System.out.println ("Résultat de l'opération :" + resultat);
-
+        double resultat = operation.calculer(nombre1, nombre2);
+        System.out.println("Résultat de l'opération : " + resultat);
     }
 }
-
-
-
